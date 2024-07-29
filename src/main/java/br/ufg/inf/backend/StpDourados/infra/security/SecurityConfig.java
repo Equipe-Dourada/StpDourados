0e38d35e95d6a4cbf7b6ae6e447ef000fd979184
JoseVazFilho <vazfilho@ufg.br>
2024-07-29 17:56:03 -0300

    Habilitando o H2-console

--- src/main/java/br/ufg/inf/backend/StpDourados/infra/security/SecurityConfig.java
+++ src/main/java/br/ufg/inf/backend/StpDourados/infra/security/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig {
 	                .authorizeHttpRequests(authorize -> authorize
 	                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 	                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+	                        .requestMatchers(HttpMethod.GET, "/h2-console/*").permitAll()
+	                        .requestMatchers(HttpMethod.POST, "/h2-console/login.do*").permitAll()
 	                        .anyRequest().authenticated()
 	                )
 	                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
